1c0f7a1450e6e1edf1fae36be4c6973dbd847efd
leojg <leojg2091@gmail.com>
2024-07-02 21:55:27 -0300

    Initial video uploading code completed

--- backend/serializers.py
+++ backend/serializers.py
@@ -6,6 +6,7 @@ from .models import VideoClip, VideoInformation
 from django.core.exceptions import ValidationError
 from django.conf import settings
 import logging
+import json
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -24,7 +25,9 @@ class VideoClipSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
 
+        logger.debug("------------------------")
         logger.debug(validated_data)
+
         video_information_data = validated_data.pop('video_information', None)
         if video_information_data:
             for video_info_data in video_information_data:
@@ -47,17 +50,17 @@ class VideoClipSerializer(serializers.ModelSerializer):
         try:
             file_path = os.path.normpath(os.path.join(settings.MEDIA_ROOT, video.name))
 
-            logger.debug(file_path)
-
             probe = ffmpeg.probe(filename=file_path)
-    
-            video_info = next(s for s in probe['streams'] if s['codec_type'] == 'video')
-            object_name = video_info.get('tags', {}).get('object_name', '')
-            detection_confidence = float(video_info.get('tags', {}).get('detection_confidence', 0.0))
 
-            return [
-                {"object_name": object_name, "detection_confidence": detection_confidence}
-            ]
+            metadata = probe.get('format', {}).get('tags', {})
+    
+            detected_objects = json.loads(metadata['description'])
+
+            logger.debug(detected_objects)
+            logger.debug("-----------------")
+
+            return detected_objects
+
         except Exception as e:
             logger.error(e)
             logger.error(e.stderr)
